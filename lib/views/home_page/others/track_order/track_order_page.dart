ca6f7c0447e1015ec7085e09bdc08010ba48c89d
Tuhin <tuhinroy881@gmail.com>
2022-09-15 19:44:14 +0530

    Order summary

--- lib/views/home_page/others/track_order/track_order_page.dart
+++ lib/views/home_page/others/track_order/track_order_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:eat_incredible_app/utils/barrel.dart';
+import 'package:eat_incredible_app/widgets/banner/custom_banner.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class TrackOrderPage extends StatelessWidget {
@@ -7,6 +10,7 @@ class TrackOrderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
@@ -40,45 +44,43 @@ class TrackOrderPage extends StatelessWidget {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 5.w, top: 3.h),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Order Summary",
-                          style: TextStyle(
-                              color: const Color.fromRGBO(97, 97, 97, 1),
-                              fontSize: 17.sp,
-                              fontWeight: FontWeight.w700)),
-                      const SizedBox(height: 10),
-                      Text("Arrived at 11:32 pm",
-                          style: TextStyle(
-                              color: const Color.fromARGB(171, 97, 97, 97),
-                              fontSize: 10.sp,
-                              fontWeight: FontWeight.w400)),
-                      const SizedBox(height: 10),
-                      Text("STATUS",
-                          style: TextStyle(
-                              color: const Color.fromRGBO(97, 97, 97, 1),
-                              fontSize: 9.sp,
-                              fontWeight: FontWeight.w400)),
-                    ],
-                  ),
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 17.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Order Summary",
+                style: GoogleFonts.poppins(
+                  color: const Color(0xff000000),
+                  fontSize: 21.sp,
+                  fontWeight: FontWeight.w600,
                 ),
-                const SizedBox(height: 10),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 3.w),
-              child: Column(
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "Arrived at 11:32 pm",
+                style: GoogleFonts.poppins(
+                  color: const Color(0xff787878),
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "STATUS",
+                style: GoogleFonts.poppins(
+                  color: const Color(0xff000000),
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: 10),
+              /*Column(
                 children: [
                   SizedBox(
-                    height: 6.h,
+                    height: 30.h,
                     child: TimelineTile(
                       lineXY: 0.1,
                       isFirst: true,
@@ -89,7 +91,8 @@ class TrackOrderPage extends StatelessWidget {
                       ),
                       endChild: Text("Order Confirmed",
                           style:
-                              GoogleFonts.poppins(fontWeight: FontWeight.w400)),
+                              GoogleFonts.poppins(fontWeight: FontWeight.w600, color: const Color(0xff000000),
+                                  fontSize: 14.sp,)),
                       beforeLineStyle: const LineStyle(
                         color: Color(0xFF27AA69),
                         thickness: 2.5,
@@ -97,64 +100,151 @@ class TrackOrderPage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
-                    height: 6.h,
+                    height: 40.h,
                     child: TimelineTile(
                       lineXY: 0.1,
                       indicatorStyle: const IndicatorStyle(
                         width: 13,
-                        color: Color(0xFF27AA69),
+                        color: Color(0xFFD9D9D9),
                         padding: EdgeInsets.all(6),
                       ),
                       endChild: Text("Shipped",
-                          style:
-                              GoogleFonts.poppins(fontWeight: FontWeight.w400)),
+                            style:
+                              GoogleFonts.poppins(fontWeight: FontWeight.w500, color: const Color(0xff000000),
+                                  fontSize: 12.sp,)),
                       beforeLineStyle: const LineStyle(
-                        color: Color(0xFF27AA69),
+                        color: Color(0xFFD9D9D9),
                         thickness: 2.5,
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 6.h,
+                    height: 40.h,
                     child: TimelineTile(
                       lineXY: 0.1,
                       indicatorStyle: const IndicatorStyle(
                         width: 13,
-                        color: Color(0xFF27AA69),
+                        color: Color(0xFFD9D9D9),
                         padding: EdgeInsets.all(6),
                       ),
                       endChild: Text("Out For Delivery",
                           style:
-                              GoogleFonts.poppins(fontWeight: FontWeight.w400)),
+                              GoogleFonts.poppins(fontWeight: FontWeight.w500, color: const Color(0xff000000),
+                                  fontSize: 12.sp,)),
                       beforeLineStyle: const LineStyle(
-                        color: Color(0xFF27AA69),
+                        color: Color(0xFFD9D9D9),
                         thickness: 2.5,
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 6.h,
+                    height: 40.h,
                     child: TimelineTile(
                       isLast: true,
                       lineXY: 0.1,
                       indicatorStyle: const IndicatorStyle(
                         width: 13,
-                        color: Color(0xFF27AA69),
+                        color: Color(0xFFD9D9D9),
                         padding: EdgeInsets.all(6),
                       ),
                       endChild: Text("Delivered",
-                          style:
-                              GoogleFonts.poppins(fontWeight: FontWeight.w400)),
+                            style:
+                              GoogleFonts.poppins(fontWeight: FontWeight.w500, color: const Color(0xff000000),
+                                  fontSize: 12.sp,)),
                       beforeLineStyle: const LineStyle(
-                        color: Color(0xFF27AA69),
+                        color: Color(0xFFD9D9D9),
                         thickness: 2.5,
                       ),
                     ),
                   ),
                 ],
-              ),
-            )
-          ],
+              )*/
+
+               Card(
+                 child: Container(
+                   height: 1,
+                   color: Colors.grey,
+                 ),
+               ),
+               SizedBox(height: 30,),
+              //!======================================================================================
+              ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 60,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom:23.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 70.h,
+                            width: 70.w,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffB50000).withOpacity(0.1),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: CustomPic(
+                              height: 70.h,
+                              width: 70.w,
+                              imageUrl:
+                                  "https://img.freepik.com/free-photo/overhead-view-fresh-broccoli-brown-basket-white-table_140725-142300.jpg?w=1060&t=st=1663250304~exp=1663250904~hmac=1e58ba0b229bf70163d4cad58c295877ac7f30b35f537c832df26b9298ea62be",
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.w,
+                          ),
+                          SizedBox(
+                            height: 70.h,
+                            width: 240.w,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Onion",
+                                style: GoogleFonts.poppins(
+                                  color:  Color(0xff2C2C2C),
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "500 g x 2",
+                                       style: GoogleFonts.poppins(
+                                  color:  Color(0xff949494),
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                    ),
+                                    Text(
+                                      "₹1.23",
+                                     style: GoogleFonts.poppins(
+                                  color:  Color(0xff000000),
+                                  fontSize: 12.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Spacer(),
+                        ],
+                      ),
+                    );
+                  }),
+
+//!=================================================================================================
+            ],
+          ),
         ),
       ),
     );
